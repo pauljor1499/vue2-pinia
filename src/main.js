c5f47d0af572cf2cd40afe6310d16279d10a734a
Paul J <jorpaulvincent@gmail.com>
2022-11-19 11:20:03 +0800

    update main file

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import { createPinia } from "pinia";
+import { createPinia, PiniaVuePlugin } from "pinia";
 
-Vue.use(createPinia());
+Vue.use(PiniaVuePlugin);
+const pinia = createPinia();
 
 Vue.config.productionTip = false;
 
 new Vue({
     router,
     vuetify,
+    pinia,
     render: (h) => h(App),
 }).$mount("#app");
